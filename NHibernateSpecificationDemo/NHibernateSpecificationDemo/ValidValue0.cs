8d9f7a95475236a5b0431cbba3b53caba7481d85
kosov.anton.rodionovich@gmail.com <kosov.anton.rodionovich@gmail.com@445254ea-d6d3-b54a-5b7b-823321a852f4>
2011-09-30 20:42:42 +0000

    simple specification

---
+++ NHibernateSpecificationDemo/NHibernateSpecificationDemo/ValidValue0.cs
@@ -0,0 +1,24 @@
+﻿namespace NHibernateSpecificationDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using LinqSpecs;
+
+    public sealed class ValidValue0 : Specification<Entity>
+    {
+        private readonly int value;
+
+        public ValidValue0(int value)
+        {
+            this.value = value;
+        }
+
+        public override System.Linq.Expressions.Expression<Func<Entity, bool>> IsSatisfiedBy()
+        {
+            return x => x.Value0 == this.value;
+        }
+    }
+}
